e51c48dc9598e99742cfdb3bf51840753d79c731
Jason Cox <j_cox@bigpond.com>
2015-03-19 18:55:05 +1100

    linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # == Class: gitweb
 #
-# Installs a gitolite and gitweb server that uses ssh to connect for read write and a website for browsing the repos
+# Installs a gitolite and gitweb server that uses ssh to connect
+# for read write and a website for browsing the repos
 #
 # === Parameters
 # 
@@ -41,12 +42,12 @@ class gitweb (
   $git_home        = '/home/git',
   $auto_tag_serial = false,
 ){
- 
+
   $git_root = "${git_home}/repositories"
   $hook     = "${git_home}/.gitolite/hooks/common"
- 
+
   if ( $git_key == undef){
-    fail("missing administrators key for gitolite")
+    fail('missing administrators key for gitolite')
   }
   if ( $auto_tag_serial == true ){
     @file {'hook post-receive-commitnumbers':
@@ -56,12 +57,12 @@ class gitweb (
     }
   } else {
     @file {'remove hook post-receive-commitnumbers':
-      name   => "${hook}/post-receive-commitnumbers",
       ensure => absent,
+      name   => "${hook}/post-receive-commitnumbers",
       tag    => 'auto_tag_serial'
     }
   }
- 
+
   include epel
 
   Package{
@@ -71,14 +72,14 @@ class gitweb (
     mode    => '0700',
     owner   => 'git',
     group   => 'git',
-  } 
+  }
   package {'gitweb': } ->
   package {'httpd': } ->
   package {'gitolite' : } ->
   package {'gitolite3' : } ->
   user { 'git' :
-    comment    => 'git user',
     ensure     => present,
+    comment    => 'git user',
     managehome => true,
     home       => $git_home,
   } ->
@@ -93,19 +94,19 @@ class gitweb (
     notify  => Service['httpd'],
     owner   => 'root',
     group   => 'root',
-    mode    => '755',
+    mode    => '0755',
   } ->
   file {'git installer':
     name    => "${git_home}/setup.sh",
     content => template("${module_name}/setup.sh.erb"),
   } ->
-  exec {'install gitolite': 
+  exec {'install gitolite':
     cwd     => $git_home,
     path    => '/usr/bin:/bin',
     command => "${git_home}/setup.sh",
     user    => 'git',
     creates => "${git_home}/.gitolite"
-  } -> 
+  } ->
   file {'hook functions':
     name    => "${hook}/functions",
     content => template("${module_name}/functions.erb"),
